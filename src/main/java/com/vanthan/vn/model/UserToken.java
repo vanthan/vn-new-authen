8c2db5981c6608a39e7ab3d70347b67ae7560f01
nttinh1 <nttinh1@cmcglobal.vn>
2022-09-30 14:25:39 +0700

    saveToken

--- src/main/java/com/vanthan/vn/model/UserToken.java
+++ src/main/java/com/vanthan/vn/model/UserToken.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 @Entity
 @Table(name = "vn_token")
 @Data
-public class Token {
+public class UserToken {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private int id;
